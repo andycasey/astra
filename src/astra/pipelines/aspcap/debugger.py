1f54bef3e096d1bbb5b911f125eb043d1b5dd6f3
Andy Casey <andycasey@gmail.com>
2026-05-14 10:51:32 -0400

    Refactor log file path in debugger function
    Updated log file path to use expand_path utility.

--- src/astra/pipelines/aspcap/debugger.py
+++ src/astra/pipelines/aspcap/debugger.py
@@ -2,9 +2,9 @@ import numpy as np
 
 RAND = np.random.randint(0, 1000)
 from socket import gethostname
-
+from astra.utils import expand_path
 HOSTNAME = gethostname()
 
 def debugger(*foo):
-    with open(f"/scratch/general/nfs1/u6020307/pbs/{HOSTNAME}-{RAND}.log", "a") as fp:
+    with open(expand_path(f"$PBS/{HOSTNAME}-{RAND}.log"), "a") as fp:
         fp.write(" ".join(map(str, foo)) + "\n")

4a518d52b8fe54ee715cbc03afa922f845d32786
imedan <ilija.medan@gmail.com>
2026-07-23 14:26:45 -0600

    type

--- src/astra/migrations/apogee.py
+++ src/astra/migrations/apogee.py
@@ -764,7 +764,7 @@ def migrate_apogee_visits(
             Visit.on_target,
             Visit.valid,
             Visit.snr,
-            fn.COALESCE(sq.c.visitflag, Visit.starflag).alias("visit_flags"),  # if no RV calculated, include flag from the visit
+            fn.COALESCE(sq.c.visitflag, Visit.visitflag).alias("visit_flags"),  # if no RV calculated, include flag from the visit
             Visit.ra.alias("input_ra"),
             Visit.dec.alias("input_dec"),
 
